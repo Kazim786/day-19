393f8fcb2b707283715b96c08d3935a1f86e8e37
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-12 16:44:36 -0600

    changed the shape

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ screen = Screen()
 # end of example
 
 #Assignment - Etch a Sketch App :
+
 # W = Forwards
 
 # S = Backwards
@@ -32,35 +33,54 @@ screen = Screen()
 
 
 
-def move_forwards():
-    t.forward(30)
+# def move_forwards():
+#     t.forward(30)
 
-screen.onkey("w", move_forwards)
-
-
-def move_backwards():
-    t.backward(10)
-
-screen.onkey(key= "S", fun= move_backwards)
-
-def move_counterclockwise():
-    t.left(10)
-
-screen.onkey(key= "A", fun= move_counterclockwise)
-
-def move_clockwise():
-    t.right(10)
-
-screen.onkey(key= "D", fun= move_clockwise)
+# screen.onkey("w", move_forwards)
 
 
-def clear():
-    t.clear()
+# def move_backwards():
+#     t.backward(10)
 
-screen.onkey(key= "C", fun= clear)
+# screen.onkey(key= "S", fun= move_backwards)
+
+# def move_counterclockwise():
+#     t.left(10)
+
+# screen.onkey(key= "A", fun= move_counterclockwise)
+
+# def move_clockwise():
+#     t.right(10)
+
+# screen.onkey(key= "D", fun= move_clockwise)
 
 
-#Assignment is done
+# def clear():
+#     t.clear()
+
+# screen.onkey(key= "C", fun= clear)
+
+
+#Assignment 1 is done
+#for turtle racing game we are probably going to use setheading, forward, random, and of course an event listener
+
+timmy = Turtle()
+timmy.color("green")
+timmy.shape("turtle")
+
+angelo = Turtle()
+angelo.color("black")
+angelo.shape("turtle")
+
+rambo = Turtle()
+rambo.color("blue")
+rambo.shape("turtle")
+
+
+lee = Turtle()
+lee.color("yellow")
+lee.shape("turtle")
+
 
 
 
